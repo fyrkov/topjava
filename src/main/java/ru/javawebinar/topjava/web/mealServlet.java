a9510c44e093b69b2336a2d163b429930e1eb59f
fyrkov <fyrkov86@gmail.com>
2016-12-14 18:53:37 +0300

    HW1 done -JSTL -sync

--- src/main/java/ru/javawebinar/topjava/web/mealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/mealServlet.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealsMemoryDAO;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.model.MealsMemoryDAO.getMealsMemoryDAO;
@@ -18,19 +24,75 @@ import static ru.javawebinar.topjava.util.MealsUtil.*;
  */
 public class mealServlet extends HttpServlet {
 
+    MealsMemoryDAO dao = getMealsMemoryDAO();
+
     private static final Logger LOG = getLogger(UserServlet.class);
-
-    /*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }*/
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
         PrintWriter pw = response.getWriter();
         //pw.println("This is mealServlet");
-        request.setAttribute("mealsMemorylist", getWithExceeded(getMealsMemoryDAO().readAll(), 2000));
-        LOG.debug("redirect to /meals.jsp with mealsMemorylist as Attribute");
-        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        //request.setAttribute("mealsMemorylist", getWithExceeded(getMealsMemoryDAO().readAll(), 2000));
+        //LOG.debug("redirect to /meals.jsp with mealsMemorylist as Attribute");
+        //request.getRequestDispatcher("/meals.jsp").forward(request, response);
 
+        String forward;
+        //String action = null;
+        String action = request.getParameter("action");
+
+        if (action!=null && action.equalsIgnoreCase("delete")){
+            int mealId = Integer.parseInt(request.getParameter("mealId"));
+            dao.delete(mealId);
+            forward = "/meals.jsp";
+            //request.setAttribute("mealsMemorylist", getWithExceeded(dao.readAll(), 2000));
+        }  else if (action!=null && action.equalsIgnoreCase("edit")){
+            int mealId = Integer.parseInt(request.getParameter("mealId"));
+            Meal meal = dao.read(mealId);
+            request.setAttribute("meal", meal);
+            forward = "/meals.jsp";
+        } else {
+            forward = "/meals.jsp";
+            //request.setAttribute("mealsMemorylist", getWithExceeded(dao.readAll(), 2000));
+        }
+        request.getSession().setAttribute("mealsMemorylist", getWithExceeded(dao.readAll(), 2000));
+        request.getRequestDispatcher(forward).forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        String dateString = request.getParameter("dateTime");
+        String parts[] = dateString.split("-|T|:");
+        for (String part: parts) {
+            System.out.println(part);
+        }
+        LocalDateTime date = LocalDateTime.of(Integer.parseInt(parts[0]),
+                Integer.parseInt(parts[1]),
+                Integer.parseInt(parts[2]),
+                Integer.parseInt(parts[3]),
+                Integer.parseInt(parts[4]));
+
+        String description = request.getParameter("description");
+
+        int cal = 0;
+        try {
+            cal = Integer.parseInt(request.getParameter("calories"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        int id = -1;
+        try {
+            id = Integer.parseInt(request.getParameter("id"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        if (id == -1) dao.create(date, description, cal);
+        else dao.update(id,date,description,cal);
+
+        String forward="/meals.jsp";
+        request.getSession().setAttribute("mealsMemorylist", getWithExceeded(dao.readAll(), 2000));
+        request.getRequestDispatcher(forward).forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,33 +1,59 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page import="java.util.List" %>
-<%@ page import="ru.javawebinar.topjava.model.MealWithExceed" %><%--
+<%@ page import="ru.javawebinar.topjava.model.MealWithExceed" %>
+<%@ page import="ru.javawebinar.topjava.model.Meal" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %><%--
   Created by IntelliJ IDEA.
   User: user
   Date: 12.12.2016
   Time: 12:34
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+
 <html>
 <head>
     <title>Meals list</title>
 </head>
 <body>
 <h2><a href="index.html">Home</a></h2>
+
+<form method="POST" action="meals" name="frmAddMeal">
+    ID : <input
+        type="text" readonly="readonly" name="id"
+        value="<c:out value="${meal.getId()}" />" /> <br />
+    Дата и время: <input
+        type="datetime-local" name="dateTime"
+        value="<c:out value="${meal.getDateTime()}" />" /> <br />
+    Описание : <input
+        type="text" name="description"
+        value="<c:out value="${meal.getDescription()}" />" /> <br />
+    Калорийность : <input
+        type="number" name="calories"
+        value="<c:out value="${meal.getCalories()}" />" /> <br />
+    <input
+        type="submit" value="Submit" />
+</form>
+
 <h2>Список еды</h2>
 <table style="height: 86px;" border="1" width="608" cellspacing="1" cellpadding="10">
     <tbody>
     <tr>
+        <th>ID</th>
         <th>Время</th>
         <th>Описание</th>
         <th>Калории</th>
     </tr>
-        <% List<MealWithExceed> list = (List<MealWithExceed>) request.getAttribute("mealsMemorylist");
+        <% List<MealWithExceed> list = (List<MealWithExceed>) request.getSession().getAttribute("mealsMemorylist");
         for (MealWithExceed meal : list) { %>
     <tr>
         <% if (meal.isExceed()) {%> <tr style="background-color:#ffcccc;"> <% } else {%> <tr style="background-color:#e6ffe6;"> <%} %>
+        <td><%=meal.getId()%></td>
         <td><%=meal.getDate()%> &nbsp;<%=meal.getTime()%></td>
         <td><%=meal.getDescription()%> </td>
         <td><%=meal.getCalories()%> </td>
+        <td><a href="meals?action=edit&mealId=<c:out value="<%=meal.getId()%>"/>">Update</a></td>
+        <td><a href="meals?action=delete&mealId=<c:out value="<%=meal.getId()%>"/>">Delete</a></td>
     </tr>
         <%}
         %>
